eec9c729370d0633feaf076762d8275a42d6e31d
Inder Dhir <inderdhir@hotmail.com>
2018-12-06 16:10:50 -0500

    Add documentation

--- Example/HoldToClickButton/ViewController.swift
+++ Example/HoldToClickButton/ViewController.swift
@@ -11,18 +11,6 @@ import HoldToClickButton
 
 class ViewController: UIViewController {
 
-//    private let holdToClickStatefulButton: HoldToClickStatefulButton = {
-//        let button = HoldToClickStatefulButton()
-//        button.backgroundColor = .clear
-//        button.titleLabel?.font = UIFont.boldSystemFont(ofSize: 16)
-//        button.setTitle("Hold to Click With States", for: .normal)
-//        button.setTitleColor(.black, for: .normal)
-//        button.layer.borderWidth = 2
-//        button.layer.borderColor = UIColor.black.cgColor
-//        button.layer.masksToBounds = true
-//        return button
-//    }()
-
     private let holdToClickButton: HoldToClickButton = {
         let button = HoldToClickButton(fillColor: .orange)
         button.backgroundColor = .clear
@@ -38,25 +26,17 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-//        view.addSubview(holdToClickStatefulButton)
         view.addSubview(holdToClickButton)
-
-//        holdToClickStatefulButton.translatesAutoresizingMaskIntoConstraints = false
-//        holdToClickStatefulButton.widthAnchor.constraint(equalToConstant: 250).isActive = true
-//        holdToClickStatefulButton.heightAnchor.constraint(equalToConstant: 50).isActive = true
-//        holdToClickStatefulButton.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
-//        holdToClickStatefulButton.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -180).isActive = true
 
         holdToClickButton.translatesAutoresizingMaskIntoConstraints = false
         holdToClickButton.widthAnchor.constraint(equalToConstant: 250).isActive = true
         holdToClickButton.heightAnchor.constraint(equalToConstant: 50).isActive = true
         holdToClickButton.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
-        holdToClickButton.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -100).isActive = true
+        holdToClickButton.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
     }
 
     override func viewDidLayoutSubviews() {
         super.viewDidLayoutSubviews()
-//        holdToClickStatefulButton.layer.cornerRadius = holdToClickStatefulButton.frame.height / 2
         holdToClickButton.layer.cornerRadius = holdToClickButton.frame.height / 2
     }
 }
--- HoldToClickButton/Classes/HoldToClickButton.swift
+++ HoldToClickButton/Classes/HoldToClickButton.swift
@@ -7,10 +7,19 @@
 
 import UIKit
 
+/// Callbacks for HoldToClickButton
 public protocol HoldToClickButtonDelegate: class {
+    
+    /// User began holding the button to click
     func didStartHoldingToClick()
+
+    /// User stopped holding the button to click
     func didStopHoldingToClick()
+
+    /// User completed the hold to click
     func didCompleteHoldToClick()
+
+    /// User cancelled the hold to click
     func didCancelHoldToClick()
 }
 
@@ -18,56 +27,77 @@ public class HoldToClickButton: UIButton {
 
     // MARK: Public
 
+    /// Delegate for callbacks
     public weak var delegate: HoldToClickButtonDelegate?
+
+    /// Duration of the progress animation. Default: 1.5
+    public var animationDuration: TimeInterval = 1.5
+
+    /// Options for progress animation. Default: curveEaseInOut
+    public var animationOptions: UIView.AnimationOptions = .curveEaseInOut
 
     // MARK: Private
 
-    private var trailing: NSLayoutConstraint!
+    /// The trailing constraint of the progressView, used to animate progress
+    private var trailingConstraint: NSLayoutConstraint!
 
+    /// Keeps track of whether the button is being held or not. Used to trigger progress and animations
     private var isHolding = false {
         didSet {
             if isHolding {
-                trailing.isActive = true
-                UIView.animate(withDuration: 1.5, delay: 0, options: .curveEaseInOut, animations: {
-                    self.layoutIfNeeded()
-                }, completion: { [weak self] completed in
-                    if completed {
-                        self?.delegate?.didCompleteHoldToClick()
-                    } else {
-                        self?.playCancelAnimation()
-                        self?.delegate?.didCancelHoldToClick()
-                    }
-                })
+                delegate?.didStartHoldingToClick()
+
+                layoutIfNeeded()
+
+                trailingConstraint.isActive = true
+                UIView.animate(
+                    withDuration: animationDuration,
+                    delay: 0,
+                    options: animationOptions,
+                    animations: { self.layoutIfNeeded() },
+                    completion: { [weak self] completed in
+                        if completed {
+                            self?.delegate?.didCompleteHoldToClick()
+                        } else {
+                            self?.playCancelAnimation()
+                            self?.delegate?.didCancelHoldToClick()
+                        }
+                    })
             } else {
-                trailing.isActive = false
-                fillView.layer.removeAllAnimations()
+                delegate?.didStopHoldingToClick()
+
+                trailingConstraint.isActive = false
+                progressView.layer.removeAllAnimations()
                 setNeedsLayout()
             }
         }
     }
 
-    private var fillView = UIView()
+    /// View used as a background to show progress while holding the button
+    private var progressView = UIView()
 
     public init(fillColor: UIColor) {
         super.init(frame: .zero)
 
-        fillView.backgroundColor = fillColor
+        progressView.backgroundColor = fillColor
 
-        addSubview(fillView)
-        sendSubview(toBack: fillView)
+        addSubview(progressView)
+        sendSubview(toBack: progressView)
 
-        fillView.translatesAutoresizingMaskIntoConstraints = false
-        fillView.leadingAnchor.constraint(equalTo: leadingAnchor).isActive = true
-        fillView.topAnchor.constraint(equalTo: topAnchor).isActive = true
-        fillView.bottomAnchor.constraint(equalTo: bottomAnchor).isActive = true
-        trailing = fillView.trailingAnchor.constraint(equalTo: trailingAnchor)
-        trailing.isActive = false
+        progressView.translatesAutoresizingMaskIntoConstraints = false
+        progressView.leadingAnchor.constraint(equalTo: leadingAnchor).isActive = true
+        progressView.topAnchor.constraint(equalTo: topAnchor).isActive = true
+        progressView.bottomAnchor.constraint(equalTo: bottomAnchor).isActive = true
+
+        trailingConstraint = progressView.trailingAnchor.constraint(equalTo: trailingAnchor)
+        trailingConstraint.isActive = false
     }
 
     public required init?(coder aDecoder: NSCoder) {
         super.init(coder: aDecoder)
     }
 
+    /// Play the cancel animation when the user stops holding the button
     private func playCancelAnimation() {
         let midX = center.x
         let midY = center.y
@@ -81,14 +111,10 @@ public class HoldToClickButton: UIButton {
     }
 
     override public func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        if !isHolding {
-            isHolding = true
-            delegate?.didStartHoldingToClick()
-        }
+        if !isHolding { isHolding = true }
     }
 
     override public func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
         isHolding = false
-        delegate?.didStopHoldingToClick()
     }
 }
